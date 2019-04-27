b57f60b884958d5c7938be6d981028dea363c2a0
GreenCalx <romain.schoenberger@gmail.com>
2019-04-28 00:37:53 +0200

    SHOP @GreenCalx

--- LD44/Assets/Scripts/Constants.cs
+++ LD44/Assets/Scripts/Constants.cs
@@ -17,5 +17,51 @@ public class Constants
     public const int STARTING_MONEY = 100;
 
     // PLAYER SAVE SLOT LABELS
-    public const string bank_account  = "BANK_ACC"; 
+    public const string bank_account  = "BANK_ACC";
+
+    // SHOP ITEMS 
+    // UI NAMES
+    public const string shopitem_name       = "Name";
+    public const string shopitem_price      = "Price";
+    public const string shopitem_infotext   = "InfoText";
+    public const string shopitem_infoimage  = "InfoImage";
+
+
+    // < name >
+    public static List<string> items = new List<string>
+    { "Monster A", "Monster B", "Monster C", "Trap A", "Freedom" };
+
+    // <name, value>
+    public static Dictionary<string, int> shopItems_Price =
+    new Dictionary<string, int>
+    {
+        { items[0], 50 },
+        { items[1], 125 },
+        { items[2], 210 },
+        { items[3], 75 },
+        { items[4], 500 }
+    };
+
+    // <name, info_text>
+    public static Dictionary<string, string> shopItems_InfoText =
+    new Dictionary<string, string>
+    {
+        { items[0], " MONSTER A INFO TEXT " },
+        { items[1], " MONSTER B INFO TEXT " },
+        { items[2], " MONSTER C INFO TEXT " },
+        { items[3], " TRAP A INFO TEXT " },
+        { items[4], " FREEDOM INFO TEXT " }
+    };
+
+    // <name, info_image_url>
+    public static Dictionary<string, string> shopItems_InfoImage =
+    new Dictionary<string, string>
+    {
+        { items[0], "" },
+        { items[1], "" },
+        { items[2], "" },
+        { items[3], "" },
+        { items[4], "" }
+    };
+    // ===================================================================================
 }
