23a1fdd96d0e46c2a23cb6bb4b8ab239ab1adf8d
Toffanim <marc.toffanin@gmail.com>
2019-04-27 21:58:48 +0200

    TextMashPro for scrollingtext

---
+++ LD44/Assets/Scripts/GUI/TextScrollingAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextScrollingAnimation : MonoBehaviour
+{
+    public List<string> DisplayedTexts;
+    private RectTransform _RT;
+    private TextMeshProUGUI _TM;
+    public float ScrollSpeed = 10;
+    public float InBetweenTime = 5;
+    private float _CurrentTime = 1;
+    private int _CurrentIdx = 0;
+    private Vector2 _StartingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _RT = GetComponent<RectTransform>();
+        _TM = GetComponent<TextMeshProUGUI>();
+        _StartingPosition = _RT.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // RT.position = new Vector2(RT.position.x + ScrollSpeed * Time.fixedDeltaTime, RT.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        if (_CurrentTime <= 0)
+        {
+            ++_CurrentIdx;
+            if (_CurrentIdx >= DisplayedTexts.Capacity) _CurrentIdx = 0;
+            _TM.text = DisplayedTexts[++_CurrentIdx];
+            _RT.position = _StartingPosition;
+            _CurrentTime = InBetweenTime;
+        }
+        _CurrentTime -= Time.deltaTime;
+        _RT.position = new Vector2(_RT.position.x - ScrollSpeed * Time.fixedDeltaTime, _RT.position.y);
+    }
+}
