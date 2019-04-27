7eb030b5b412fdfb9084801bae38cc2e6bb73980
Toffanim <marc.toffanin@gmail.com>
2019-04-27 22:51:11 +0200

    Petit fix Weapon

--- LD44/Assets/Scripts/Player/Weapon.cs
+++ LD44/Assets/Scripts/Player/Weapon.cs
@@ -6,28 +6,36 @@ public class Weapon : MonoBehaviour
     private Camera _Camera;
     private Vector2 FireVector;
     public GameObject Bullet;
-
+    private Transform _Parent;
+    private Transform _StartingPosition;
+    public GameObject FireExit;
+    public float BulletSpeed;
     // Start is called before the first frame update
     void Start()
     {
         _Camera = FindObjectOfType<Camera>();
+        _Parent = this.transform.parent.transform;
+        _StartingPosition = this.transform;
     }
 
     public void OnFire()
     {
-        var b = Instantiate(Bullet, transform.position, transform.rotation);
+        var b = Instantiate(Bullet, FireExit.transform.position, FireExit.transform.rotation);
         var RB = b.GetComponent<Rigidbody2D>();
-        RB.velocity = new Vector2(FireVector.x,FireVector.y);
+        RB.velocity = new Vector2(FireVector.x  *BulletSpeed,FireVector.y *BulletSpeed);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         var mousePos = Input.mousePosition;
-        var screenPos = _Camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - _Camera.transform.position.z));
-        var angle = Mathf.Atan2((screenPos.y - transform.position.y), (screenPos.x - transform.position.x));
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle * Mathf.Rad2Deg);
-
-        FireVector = new Vector2( Mathf.Cos(angle), Mathf.Sin(angle) );
+        var screenPos = _Camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, _Parent.position.z - _Camera.transform.position.z));
+        var angle = Mathf.Atan2((screenPos.y - _Parent.position.y), (screenPos.x - _Parent.position.x));
+        transform.position = _StartingPosition.position;
+        transform.rotation = _StartingPosition.rotation;
+        //transform.RotateAround(_Parent.position, new Vector3(0,0,1), angle);
+        _Parent.eulerAngles = new Vector3(_Parent.eulerAngles.x, _Parent.eulerAngles.y, angle * Mathf.Rad2Deg);
+        //Debug.Log(_Parent.position + " " + new Vector3(0, 0, 1) + " " + angle * Mathf.Rad2Deg);
+         FireVector = new Vector2( Mathf.Cos(angle), Mathf.Sin(angle) );
     }
 }
