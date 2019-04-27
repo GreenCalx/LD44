0b191aef07763a14dc39d4e97cd00ea032259c24
Toffanim <marc.toffanin@gmail.com>
2019-04-28 00:23:15 +0200

    Beginning of blood splash system

---
+++ LD44/Assets/BloodSplash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplash : MonoBehaviour
+{
+    ParticleSystem PS;
+    ParticleSystem.Particle[] particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PS = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[PS.main.maxParticles];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        // GetParticles is allocation free because we reuse the m_Particles buffer between updates
+        int numParticlesAlive = PS.GetParticles(particles);
+
+        // Change only the particles that are alive
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            if(particles[i].remainingLifetime < (particles[i].startLifetime - 0.1))
+            {
+                particles[i].velocity = Vector3.zero;
+                
+            }
+        }
+        PS.SetParticles(particles, numParticlesAlive);
+    }
+}
