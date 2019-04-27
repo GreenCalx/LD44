528c8c47f6694e66f4329986b4e64b5c7ab1c2fc
GreenCalx <romain.schoenberger@gmail.com>
2019-04-28 00:57:54 +0200

    Shop++ @GreenCalx

--- LD44/Assets/ShopEnterController.cs
+++ LD44/Assets/ShopEnterController.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class ShopEnterController : MonoBehaviour
 {
     public GameObject shopParentUIGO;
+
     private ShopBehaviour shop;
+    private bool playerInShop;
 
     // Start is called before the first frame update
     void Start()
     {
         if (!!shopParentUIGO)
             shop = shopParentUIGO.GetComponentInChildren<ShopBehaviour>();
-        exitShop();
+        if (!!shop)
+            shop.gameObject.SetActive(false);
+
+        playerInShop = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Escape) && playerInShop)
+            exitShop();
     }
 
 
     public void playerOnShop()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && !playerInShop)
         {
-            print("ENTER SHOP");
+            playerInShop = true;
+
+            if (Constants.DEBUG_ENABLED)
+                print("ENTER SHOP");
             // ENTER SHOP
             if (!!shop)
                 shop.gameObject.SetActive(true);
@@ -35,8 +44,16 @@ public class ShopEnterController : MonoBehaviour
 
     public void exitShop()
     {
-        if (!!shop)
-            shop.gameObject.SetActive(false);
+        if (playerInShop)
+        {
+            if (Constants.DEBUG_ENABLED)
+                print("EXIT SHOP");
+
+            if (!!shop)
+                shop.gameObject.SetActive(false);
+
+            playerInShop = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
