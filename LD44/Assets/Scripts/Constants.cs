c350f9c1ba37f5b5d2103a57edf3b20103551ca8
GreenCalx <romain.schoenberger@gmail.com>
2019-04-27 15:00:24 +0200

    DeathZone @GreenCalx

--- LD44/Assets/Scripts/Constants.cs
+++ LD44/Assets/Scripts/Constants.cs
@@ -8,4 +8,7 @@ public class Constants
     public const string arena_scene_name    = "ARENA_SCENE";
     public const string hub_scene_name      = "HUB_SCENE";
     public const string title_scene_name    = "TITLE_SCENE";
+
+    // PLAYER STATS
+    public const int LETHAL_DAMAGE = 99;
 }
