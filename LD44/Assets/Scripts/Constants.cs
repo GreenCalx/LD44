5d10a3b471dd72d53b0eaf07fbcf5d8c65d36f64
GreenCalx <romain.schoenberger@gmail.com>
2019-04-27 14:28:57 +0200

    Scenes & title debut @GreenCalx

---
+++ LD44/Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    // SCENES
+    public const string arena_scene_name    = "ARENA_SCENE";
+    public const string hub_scene_name      = "HUB_SCENE";
+    public const string title_scene_name    = "TITLE_SCENE";
+}
