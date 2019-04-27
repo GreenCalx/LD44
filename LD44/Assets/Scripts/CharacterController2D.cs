7b5a001462a5453b67b2da71e8621380fb735d98
Toffanim <marc.toffanin@gmail.com>
2019-04-27 14:23:57 +0200

    WIP: Make Player movement
    Lack PlayerInputs

---
+++ LD44/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float _MaxSpeed = 10f;
+    private Rigidbody2D _RB;
+
+    // Awake is called as Init
+    private void Awake()
+    {
+        _RB = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float HorizontalMovement)
+    {
+        _RB.velocity = new Vector2(HorizontalMovement * _MaxSpeed, _RB.velocity.y);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LD44/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerBehavior : MonoBehaviour
+{
+    private CharacterController2D _CC2D;
+    private PlayerInput _PI;
+    private bool _IsFacingRight = true;
+
+    public void OnDamage(Damager D, Damageable DA)
+    {
+        //ParticleSystem PS = GetComponentInChildren<ParticleSystem>();
+        //PS.Play();
+    }
+
+    // Awake is called as Init
+    private void Awake()
+    {
+        _PI = GetComponent<PlayerInput>();
+        _CC2D = GetComponent<CharacterController2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _CC2D.Move(PInput.Horizontal.Value, PInput.Jump.Down);
+    }
+}
