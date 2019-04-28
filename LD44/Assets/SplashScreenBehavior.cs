3ad0d7461d6f4e5ca3ebcd1af5fc39ee07113e06
Toffanim <marc.toffanin@gmail.com>
2019-04-28 18:58:47 +0200

    Splash Screen

---
+++ LD44/Assets/SplashScreenBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SplashScreenBehavior : MonoBehaviour
+{
+    public List<string> DisplayedTexts;
+    private TextMeshProUGUI _TM;
+    private int _CurrentIdx = 0;
+
+    private void OnEnd()
+    {
+        //load scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _TM = GetComponent<TextMeshProUGUI>();
+        _TM.text = DisplayedTexts[_CurrentIdx];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            ++_CurrentIdx;
+            if (_CurrentIdx >= DisplayedTexts.Capacity) OnEnd();
+            _TM.text = DisplayedTexts[_CurrentIdx];
+        }
+    }
+}
